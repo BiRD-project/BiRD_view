4a90e016d0c1f7e437b84fbadd186be71ce83862
lanevsd1 <dmitri.lanevski@aalto.fi>
2021-07-23 18:20:08 +0300

    NB about heroku 30s time limit added

--- help_text.py
+++ help_text.py
@@ -12,7 +12,7 @@ open source library and the code can be found [here](https://github.com/BiRD-pro
 Current version of application allows to open and view files only in the file format developed and agreed by BiRD consortium.
 
 NB! Heroku servers have timeout of 30s. This may cause application to fail to upload very large files.
-The solution is known and will be presented in the next vercion of application. Thank you for understanding!
+The solution is known and will be introduced in the next version of application. Thank you for understanding!
 
 #### *Universal BRDF data format*
 BRDF file format is based on [json](https://www.json.org/json-en.html) syntax. I.e. it is a text file that stores data in dictionaries in the form of:
