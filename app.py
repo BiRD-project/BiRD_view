b8ed926af6bf04644e6ad03111b5d03db7f33943
Robin-Aschan <74412263+Robin-Aschan@users.noreply.github.com>
2024-11-21 11:09:43 +0200

    BRDF VZA fix
    Viewing zenith angles did not properly show in the bottom right quadrant when uploading a file. the "x" variable for angle was split in two for negative and positve angles.

--- app.py
+++ app.py
@@ -1435,10 +1435,10 @@ def update_2D_brdf_plot(trigger1, trigger2, trigger3, uploaded_data, filename, s
     # ubrdf = np.array([])
     theta_r = np.array([])
     phi_r = np.array([])
-    s_phi_r = 0
+    s_phi_r = 0 #selected phi_r in the app
     for variable in data:
         if variable != 'BRDF' and variable != 'uBRDF' and variable != 'theta_r' and variable != 'phi_r':
-            if mask.size == 0:
+            if mask.size == 0: 
                 mask = np.array(data[variable]['values']) == data[variable]['sval']
             else:
                 mask = np.logical_and(mask, np.array(data[variable]['values']) == data[variable]['sval'])
@@ -1456,7 +1456,8 @@ def update_2D_brdf_plot(trigger1, trigger2, trigger3, uploaded_data, filename, s
     brdf = brdf[mask]
     # ubrdf = ubrdf[mask]
     theta_r = theta_r[mask]
-    phi_r = phi_r[mask]
+    phi_pre_mask = phi_r
+    phi_r = phi_pre_mask[mask]
 
     figure = go.Figure()
 
@@ -1535,16 +1536,18 @@ def update_2D_brdf_plot(trigger1, trigger2, trigger3, uploaded_data, filename, s
     elif selected_phiVs.shape[0] == 2:
         y_1 = y[phis[phi_mask] == selected_phiVs[0]]
         y_2 = y[phis[phi_mask] == selected_phiVs[1]]
+        x_1 = x[phis[phi_mask] == selected_phiVs[0]]
+        x_2 = x[phis[phi_mask] == selected_phiVs[1]]
         if selected_phiVs[0] < 180:
-            y_1 = y_1[np.argsort(-x)]
-            y_2 = y_2[np.argsort(x)]
+            y_1 = y_1[np.argsort(-x_1)]
+            y_2 = y_2[np.argsort(x_2)]
             y = np.concatenate((y_1, y_2))
-            x = np.concatenate((np.sort(-x), np.sort(x)))
+            x = np.concatenate((np.sort(-x_1), np.sort(x_2)))
         else:
             y_1 = y_1[np.argsort(x)]
             y_2 = y_2[np.argsort(-x)]
             y = np.concatenate((y_1, y_2))
-            x = np.concatenate((np.sort(-x), np.sort(x)))
+            x = np.concatenate((np.sort(-x_1), np.sort(x_2)))
         # x[y < 0] = None
         y[y < 0] = None
         figure.add_trace(go.Scatter(name='BRDF',
